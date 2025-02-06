63e52c16a9b04f110808975137bbe527bcb08d70
jia.xia <xiajia@vip.qq.com>
2025-02-06 08:06:18 +0800

    chore: update lerna config and commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
-};
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      ['server', 'web', 'ui', 'shared'] // 允许的作用域列表
+    ]
+  }
+}
